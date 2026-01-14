8c2dc7f1af244e9f9c6a1ae8d308a3bc8637f34e
Administrator <safet.srna@fpmoz.sum.ba>
2026-01-14 14:37:48 +0100

    Studentski sustav za upravljanje projektima i zadatcima

---
+++ src/main/java/Studentski/sustav/sustav_backend/SustavBackendApplication.java
@@ -0,0 +1,14 @@
+package Studentski.sustav.sustav_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustavBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SustavBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Studentski/sustav/sustav_backend/SustavBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Studentski.sustav.sustav_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustavBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
